c8e71891f7aeaa06c23ade371368dbfedde7133d
Dennis Jason <jasonden98@gmail.com>
2022-01-22 10:34:04 +0700

    Add Bottom Navigator and font assets

--- LaundryOnlineApp/src/router/index.js
+++ LaundryOnlineApp/src/router/index.js
@@ -5,13 +5,14 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import { Home, Splash, Akun, Pesanan } from '../pages';
+import { BottomNavigator } from '../components';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const MainApp = () => {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator tabBar={props => <BottomNavigator {...props} />}>
             <Tab.Screen name="Home" component={Home} options={{ headerShown: false }}/>
             <Tab.Screen name="Pesanan" component={Pesanan} options={{ headerShown: false }}/>
             <Tab.Screen name="Akun" component={Akun} options={{ headerShown: false }}/>
