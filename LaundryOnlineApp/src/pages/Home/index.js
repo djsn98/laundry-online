639670b38a568ae290e0d60d3b281cd53ed1f6ff
Dennis Jason <jasonden98@gmail.com>
2022-01-22 11:00:39 +0700

    Add header background

--- LaundryOnlineApp/src/pages/Home/index.js
+++ LaundryOnlineApp/src/pages/Home/index.js
@@ -1,14 +1,49 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Dimensions, Image, ImageBackground, StyleSheet, Text, View } from 'react-native'
+import { ImageHeader, Logo } from '../../assets'
 
 const Home = () => {
     return (
-        <View>
-            <Text>Beranda</Text>
+        <View style={styles.page}>
+            <ImageBackground source={ImageHeader} style={styles.header}>
+                <Image source={Logo} style={styles.logo} />
+                <View style={styles.hello}>
+                    <Text style={styles.selamat}>Selamat Datang,</Text>
+                    <Text style={styles.username}>Dennis Jason</Text>
+                </View>
+            </ImageBackground>
         </View>
     )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const windowWidth = Dimensions.get('window').width
+const windowHeight = Dimensions.get('window').height
+
+const styles = StyleSheet.create({
+    page: {
+        flex: 1
+    },
+    header: {
+        width: windowWidth,
+        height: windowHeight * 0.3,
+        paddingHorizontal: 30,
+        paddingTop: 10
+    },
+    logo: {
+        width: windowWidth * 0.25,
+        height: windowHeight * 0.06
+    },
+    hello: {
+        marginTop: windowHeight * 0.030
+    },
+    selamat: {
+        fontSize: 22,
+        fontFamily: 'TitilliumWeb-Regular'
+    },
+    username: {
+        fontSize: 20,
+        fontFamily: 'TitilliumWeb-Bold'
+    }
+})
