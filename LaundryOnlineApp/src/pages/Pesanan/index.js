530cac048024dded085795bb64c24f9066ce3925
Dennis Jason <jasonden98@gmail.com>
2022-01-25 13:01:18 +0700

    Add switch filter pesanan

--- LaundryOnlineApp/src/pages/Pesanan/index.js
+++ LaundryOnlineApp/src/pages/Pesanan/index.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { Searchbar } from 'react-native-paper';
-import { WARNA_ABU_ABU, WARNA_UTAMA } from '../../utils/constant'
+import { WARNA_ABU_ABU, WARNA_UTAMA } from '../../utils/constant';
+import SwitchSelector from "react-native-switch-selector";
 
 const Pesanan = () => {
     const [searchQuery, setSearchQuery] = React.useState('');
 
-    const onChangeSearch = query => setSearchQuery(query);
+    const options = [
+        { label: "Aktif", value: "1", activeColor: WARNA_UTAMA },
+        { label: "Selesai", value: "1.5", activeColor: WARNA_UTAMA },
+        { label: "Semua", value: "2", activeColor: WARNA_UTAMA }
+      ];
+
+    const onChangeSearch = (query) => {
+        console.log(query)
+        setSearchQuery(query)
+    };
 
     return (
         <View style={styles.page}>
@@ -19,6 +29,14 @@ const Pesanan = () => {
                     inputStyle={styles.placeholder}
                 />
             </View>
+            <SwitchSelector
+                style={styles.selector}
+                textStyle={styles.textUnselect}
+                selectedTextStyle={styles.textSelected}
+                options={options}
+                initial={0}
+                onPress={value => console.log(`Call onPress with value: ${value}`)}
+            />
         </View>
     )
 }
@@ -42,5 +60,15 @@ const styles = StyleSheet.create({
     placeholder: {
         fontSize: 18,
         fontFamily: 'Roboto',
-    }
+    },
+    selector: {
+        marginTop: 11,
+        marginHorizontal: 12
+    },
+    textUnselect: {
+        fontFamily: 'TitilliumWeb-SemiBold'
+    },
+    textSelected: {
+        fontFamily: 'TitilliumWeb-SemiBold'
+    },
 })
