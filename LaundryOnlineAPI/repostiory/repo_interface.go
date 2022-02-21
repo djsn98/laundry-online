28a7ddd479944c15c39d3a5d5ed28448cfc4527c
Dennis Jason <jasonden98@gmail.com>
2022-02-21 14:04:24 +0700

    Add service repo interface

---
+++ LaundryOnlineAPI/repostiory/repo_interface.go
@@ -0,0 +1,14 @@
+package repostiory
+
+import (
+	"LaundryOnlineAPI/model/core"
+	"context"
+)
+
+type ServiceRepoInterface interface {
+	Save(ctx context.Context, service *core.Service) error
+	FindAll(ctx context.Context) ([]*core.Service, error)
+	FindById(ctx context.Context, serviceId *uint) (*core.Service, error)
+	Update(ctx context.Context, service *core.Service) error
+	Destroy(ctx context.Context, serviceId *uint) error
+}
