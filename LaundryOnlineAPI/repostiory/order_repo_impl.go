08d0e31ceff9e78897185c8755d41ec7ceaa32e4
Dennis Jason <jasonden98@gmail.com>
2022-02-21 16:33:51 +0700

    Implement order repo

---
+++ LaundryOnlineAPI/repostiory/order_repo_impl.go
@@ -0,0 +1,88 @@
+package repostiory
+
+import (
+	"LaundryOnlineAPI/model/core"
+	"context"
+	"errors"
+
+	"gorm.io/gorm"
+)
+
+type OrderRepoImpl struct {
+	DB *gorm.DB
+}
+
+func (ori *OrderRepoImpl) Save(ctx context.Context, order *core.Order) error {
+	err := ori.DB.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Create(order).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ori *OrderRepoImpl) FindAll(ctx context.Context) (*[]core.Order, error) {
+	var orders []core.Order
+
+	err := ori.DB.Find(&orders).Error
+	if err != nil {
+		return &orders, err
+	}
+
+	if len(orders) == 0 {
+		return &orders, errors.New("Order not found!")
+	}
+
+	return &orders, nil
+}
+
+func (ori *OrderRepoImpl) FindById(ctx context.Context, orderId *uint) (*core.Order, error) {
+	var order core.Order
+
+	err := ori.DB.First(&order, orderId).Error
+	if err != nil {
+		return &order, err
+	}
+
+	if order.Status == "" {
+		return &order, errors.New("Customer not found!")
+	}
+
+	return &order, nil
+}
+
+func (ori *OrderRepoImpl) Update(ctx context.Context, order *core.Order) error {
+	err := ori.DB.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Save(order).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ori *OrderRepoImpl) Destroy(ctx context.Context, orderId *uint) error {
+	err := ori.DB.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Delete(&core.Order{}, orderId).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
