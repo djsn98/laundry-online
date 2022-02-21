1bb83ed946e61e2d5df425a1d936ddeab1bad3b2
Dennis Jason <jasonden98@gmail.com>
2022-02-21 15:32:50 +0700

    Change repo interface and implement repo service

--- LaundryOnlineAPI/repostiory/repo_interface.go
+++ LaundryOnlineAPI/repostiory/repo_interface.go
@@ -7,7 +7,7 @@ import (
 
 type ServiceRepoInterface interface {
 	Save(ctx context.Context, service *core.Service) error
-	FindAll(ctx context.Context) ([]*core.Service, error)
+	FindAll(ctx context.Context) (*[]core.Service, error)
 	FindById(ctx context.Context, serviceId *uint) (*core.Service, error)
 	Update(ctx context.Context, service *core.Service) error
 	Destroy(ctx context.Context, serviceId *uint) error
---
+++ LaundryOnlineAPI/repostiory/service_repo_impl.go
@@ -0,0 +1,88 @@
+package repostiory
+
+import (
+	"LaundryOnlineAPI/model/core"
+	"context"
+	"errors"
+
+	"gorm.io/gorm"
+)
+
+type ServiceRepoImpl struct {
+	DB *gorm.DB
+}
+
+func (sri *ServiceRepoImpl) Save(ctx context.Context, service *core.Service) error {
+	err := sri.DB.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Create(service).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (sri *ServiceRepoImpl) FindAll(ctx context.Context) (*[]core.Service, error) {
+	var services []core.Service
+
+	err := sri.DB.Find(&services).Error
+	if err != nil {
+		return &services, err
+	}
+
+	if len(services) == 0 {
+		return &services, errors.New("Service not found!")
+	}
+
+	return &services, nil
+}
+
+func (sri *ServiceRepoImpl) FindById(ctx context.Context, serviceId *uint) (*core.Service, error) {
+	var service core.Service
+
+	err := sri.DB.First(&service).Error
+	if err != nil {
+		return &service, err
+	}
+
+	if service == (core.Service{}) {
+		return &service, errors.New("Service not found!")
+	}
+
+	return &service, nil
+}
+
+func (sri *ServiceRepoImpl) Update(ctx context.Context, service *core.Service) error {
+	err := sri.DB.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Save(service).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (sri *ServiceRepoImpl) Destroy(ctx context.Context, serviceId *uint) error {
+	err := sri.DB.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Delete(&core.Service{}, serviceId).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
