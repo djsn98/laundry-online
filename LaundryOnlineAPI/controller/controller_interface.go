d638da2ed04b086f774cc239731f6fe311d4e21c
Dennis Jason <jasonden98@gmail.com>
2022-02-22 14:04:46 +0700

    Add controller interface

---
+++ LaundryOnlineAPI/controller/controller_interface.go
@@ -0,0 +1,27 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+type ServiceControllerInterface interface {
+	Create(c *gin.Context)
+	ReadAll(c *gin.Context)
+	ReadById(c *gin.Context)
+	Update(c *gin.Context)
+	Delete(c *gin.Context)
+}
+
+type CustomerControllerInterface interface {
+	Create(c *gin.Context)
+	ReadAll(c *gin.Context)
+	ReadByUsername(c *gin.Context)
+	Update(c *gin.Context)
+	Delete(c *gin.Context)
+}
+
+type OrderControllerInterface interface {
+	Create(c *gin.Context)
+	ReadAll(c *gin.Context)
+	ReadById(c *gin.Context)
+	Update(c *gin.Context)
+	Delete(c *gin.Context)
+}
